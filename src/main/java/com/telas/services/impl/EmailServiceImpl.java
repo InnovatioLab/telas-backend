a0f4b3a1eb271f1fc9749e91d0943ed3c10826a3
Victor Almeida <victoremmanuelmn@gmail.com>
2025-09-16 11:12:01 -0300

    inicia mudança para sendgrid

--- src/main/java/com/telas/services/impl/EmailServiceImpl.java
+++ src/main/java/com/telas/services/impl/EmailServiceImpl.java
@@ -1,5 +1,12 @@
 package com.telas.services.impl;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import com.telas.dtos.EmailDataDto;
 import com.telas.infra.exceptions.BusinessRuleException;
 import com.telas.services.EmailService;
@@ -8,55 +15,57 @@ import com.telas.shared.constants.valitation.ContactValidationMessages;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import java.io.IOException;
-import java.io.StringWriter;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
-  private final JavaMailSender emailSender;
-  private final Configuration freemarkerConfig;
+    private final Configuration freemarkerConfig;
 
-  @Value("${spring.mail.username}")
-  private String emailFrom;
+    @Value("${spring.mail.username}")
+    private String emailFrom;
 
-  @Override
-  @Async
-  public void send(EmailDataDto data) {
-    try {
-      Template template = freemarkerConfig.getTemplate(data.getTemplate());
+    @Value("${sendgrid.api.key}")
+    private String sendGridApiKey;
 
-      StringWriter stringWriter = new StringWriter();
-      template.process(data.getParams(), stringWriter);
-      String conteudoEmail = stringWriter.toString();
+    @Override
+    @Async
+    public void send(EmailDataDto data) {
+        try {
+            Template template = freemarkerConfig.getTemplate(data.getTemplate());
+            String conteudoEmail = FreeMarkerTemplateUtils.processTemplateIntoString(freemarkerConfig.getTemplate(data.getTemplate()), data.getParams());
 
-      MimeMessage mimeMessage = emailSender.createMimeMessage();
-      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-      InternetAddress sender = new InternetAddress(emailFrom, SharedConstants.EMAIL_SENDER);
-      InternetAddress destination = new InternetAddress(data.getEmail(), SharedConstants.DESTINATARIO);
+            Email from = new Email(SharedConstants.EMAIL_SENDER, "Telas");
+            Email toEmail = new Email(data.getEmail());
+            Content content = new Content("text/html", conteudoEmail);
+            Mail mail = new Mail(from, data.getSubject(), toEmail, content);
 
-      helper.setSubject(data.getSubject());
-      helper.setFrom(sender);
-      helper.setTo(destination);
-      helper.setText(conteudoEmail, true);
-      emailSender.send(mimeMessage);
-      log.info("Message sent to email {}", data.getEmail());
-    } catch (MailException | MessagingException | IOException | TemplateException e) {
-      log.error("Error while sending email: {}", e.getMessage());
-      throw new BusinessRuleException(ContactValidationMessages.ERRO_WHILE_SENDING_EMAIL);
+            SendGrid sg = new SendGrid(sendGridApiKey);
+            Request request = new Request();
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            if (response.getStatusCode() >= 400 && response.getStatusCode() <= 500) {
+                log.error("Error while sending email: {}", response.getBody());
+                throw new BusinessRuleException(ContactValidationMessages.ERRO_WHILE_SENDING_EMAIL);
+            }
+
+            log.info("Message sent to email {}", data.getEmail());
+        } catch (IOException | TemplateException ex) {
+            log.error("Error while sending email: {}", ex.getMessage());
+            throw new BusinessRuleException(ContactValidationMessages.ERRO_WHILE_SENDING_EMAIL);
+        }
     }
-  }
+
+
 }
